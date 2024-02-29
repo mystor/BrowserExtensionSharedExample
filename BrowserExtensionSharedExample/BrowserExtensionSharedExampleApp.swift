2e4010a2c635af0e9487108275b6898e49b5636e
Nika Layzell <nika@thelayzells.com>
2024-02-29 14:12:48 -0500

    Initial Commit

---
+++ BrowserExtensionSharedExample/BrowserExtensionSharedExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserExtensionSharedExampleApp.swift
+//  BrowserExtensionSharedExample
+//
+//  Created by Nika Layzell on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct BrowserExtensionSharedExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
